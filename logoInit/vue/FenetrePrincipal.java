ff91baa399bde62fd7bd4748c591d2386802b408
FaCoInf <corinne.fagno@gmail.com>
2015-04-27 17:39:13 +0200

    geter setteur element vue

--- logoInit/vue/FenetrePrincipal.java
+++ logoInit/vue/FenetrePrincipal.java
@@ -7,18 +7,22 @@ import javax.swing.JFrame;
 
 public class FenetrePrincipal extends JFrame implements Observateur {
 
+	private MenuAppli menubar;
+	private MenuHaut menuHaut;
+	private MenuBas menuBas;
+	
 	public FenetrePrincipal() {
 		super("un logo tout simple");
 		getContentPane().setLayout(new BorderLayout(10, 10));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		MenuAppli menubar = new MenuAppli();
+		menubar = new MenuAppli();
 		setJMenuBar(menubar);	// on installe le menu bar
 		
-		MenuHaut menuHaut = new MenuHaut();
+		menuHaut = new MenuHaut();
 		getContentPane().add(menuHaut, "North");
 		
-		MenuBas menuBas = new MenuBas(new GridLayout());
+		menuBas = new MenuBas(new GridLayout());
 		getContentPane().add(menuBas, "South");
 		
 		FeuilleDessin feuille = new FeuilleDessin();
@@ -27,6 +31,30 @@ public class FenetrePrincipal extends JFrame implements Observateur {
 		
 		pack();
 		setVisible(true);
+	}
+
+	public MenuAppli getMenubar() {
+		return menubar;
+	}
+
+	public void setMenubar(MenuAppli menubar) {
+		this.menubar = menubar;
+	}
+
+	public MenuHaut getMenuHaut() {
+		return menuHaut;
+	}
+
+	public void setMenuHaut(MenuHaut menuHaut) {
+		this.menuHaut = menuHaut;
+	}
+
+	public MenuBas getMenuBas() {
+		return menuBas;
+	}
+
+	public void setMenuBas(MenuBas menuBas) {
+		this.menuBas = menuBas;
 	}
 
 	@Override
